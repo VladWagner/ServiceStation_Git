63396b084be4e238f1a7a9c52623f15662471807
Vlad <vladwagner58@gmail.com>
2022-03-03 23:11:34 +0300

    Branches merged

---
+++ ServiceStationWPF/Views/TbxWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServiceStationWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for TbxWindow.xaml
+    /// </summary>
+    public partial class TbxWindow : Window
+    {
+        public TbxWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+            => Close();
+    }
+}
